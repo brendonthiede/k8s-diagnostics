5889d8eb7500ef818ea884928cb15d7aed130edb
Brendon Thiede <brendonthiede@filevine.com>
2022-12-09 22:14:06 -0500

    Removing unneeded files from pages branch

--- run-diagnostics-in-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-NAMESPACE="${1:-"default"}"
-IMAGE_TAG="${2:-"latest"}"
-
-kubectl run k8s-diagnostics --namespace "${NAMESPACE}" --rm --restart=Never -i --tty --image "thiedebr/k8s-diagnostics:${IMAGE_TAG}"
