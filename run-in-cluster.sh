3a6d15d6ac2c646b7b323f0ad5c69626b376101d
Brendon Thiede <brendon.thiede@gmail.com>
2024-09-26 22:06:36 -0400

    Adding script to run pod in cluster

---
+++ run-in-cluster.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAMESPACE="${1:-"default"}"
+IMAGE_TAG="${2:-"latest"}"
+
+kubectl run k8s-diagnostics --namespace "${NAMESPACE}" --rm --restart=Never -i --tty --image "thiedebr/k8s-diagnostics:${IMAGE_TAG}" -- /bin/bash
