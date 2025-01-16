2970876e49cd9dfe2fd6c8c34c7e7cb5e5f29032
Brendon Thiede <brendon.thiede@gmail.com>
2025-01-15 22:08:14 -0500

    Adding more security context info

--- k8s-diagnostics-helm-deploy.sh
+++ k8s-diagnostics-helm-deploy.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env bash
 
-NAMESPACE_NAME="${1}"
+SG_NAMESPACE_NAME="${1}"
+DEPLOY_NAMESPACE_NAME="${2:-"default"}"
 
-if [ -z "${NAMESPACE_NAME}" ]; then
-    NAMESPACE_NAME="$(kubectl config view --minify | grep namespace | cut -d" " -f6)"
-    if [ -z "${NAMESPACE_NAME}" ]; then
-        NAMESPACE_NAME="default"
+if [ -z "${SG_NAMESPACE_NAME}" ]; then
+    SG_NAMESPACE_NAME="$(kubectl config view --minify | grep namespace | cut -d" " -f6)"
+    if [ -z "${SG_NAMESPACE_NAME}" ]; then
+        SG_NAMESPACE_NAME="default"
     fi
 fi
 
-SECURITY_GROUPS="$(kubectl get securitygrouppolicies.vpcresources.k8s.aws -n "${NAMESPACE_NAME}" -o jsonpath='{.items[*].spec.securityGroups.groupIds[*]}' | tr ' ' '\n' | sort -u | paste -sd "," -)"
+SECURITY_GROUPS="$(kubectl get securitygrouppolicies.vpcresources.k8s.aws -n "${SG_NAMESPACE_NAME}" -o jsonpath='{.items[*].spec.securityGroups.groupIds[*]}' | tr ' ' '\n' | sort -u | paste -sd "," -)"
 
 HELM_CHART_RELEASE="k8s-diagnostics-1.1.2"
 HELM_CHART="https://github.com/brendonthiede/k8s-diagnostics/releases/download/${HELM_CHART_RELEASE}/${HELM_CHART_RELEASE}.tgz"
 
-helm upgrade --install -n "${NAMESPACE_NAME}" k8s-diagnostics "${HELM_CHART}" --set "podSecurityGroupIds={${SECURITY_GROUPS}}"
+helm upgrade --install -n "${DEPLOY_NAMESPACE_NAME}" k8s-diagnostics "${HELM_CHART}" --set "podSecurityGroupIds={${SECURITY_GROUPS}}"
 
-while ! kubectl get po -l app.kubernetes.io/instance=k8s-diagnostics -n "${NAMESPACE_NAME}" | grep '1/1'; do
+while ! kubectl get po -l app.kubernetes.io/instance=k8s-diagnostics -n "${DEPLOY_NAMESPACE_NAME}" | grep '1/1' >/dev/null; do
     sleep 1
 done
 
 sleep 2
 
 echo "[INFO] Deleting pod to get security groups applied"
-
-kubectl delete po -l app.kubernetes.io/instance=k8s-diagnostics -n "${NAMESPACE_NAME}" --wait=true
-
-while ! kubectl get po -l app.kubernetes.io/instance=k8s-diagnostics -n "${NAMESPACE_NAME}" | grep '1/1'; do
+kubectl delete po -l app.kubernetes.io/instance=k8s-diagnostics -n "${DEPLOY_NAMESPACE_NAME}" --wait=true
+while kubectl get po -l app.kubernetes.io/instance=k8s-diagnostics -n "${DEPLOY_NAMESPACE_NAME}" | grep 'Terminating' >/dev/null; do
     sleep 1
 done
 
-kubectl exec -n "${NAMESPACE_NAME}" -it "$(kubectl get po -l app.kubernetes.io/instance=k8s-diagnostics -n "${NAMESPACE_NAME}" -o name)" -- /bin/bash
+echo "[INFO] Waiting for pod to be recreated"
+while ! kubectl get po -l app.kubernetes.io/instance=k8s-diagnostics -n "${DEPLOY_NAMESPACE_NAME}" | grep '1/1' >/dev/null; do
+    sleep 1
+done
+
+kubectl exec -n "${DEPLOY_NAMESPACE_NAME}" -it "$(kubectl get po -l app.kubernetes.io/instance=k8s-diagnostics -n "${DEPLOY_NAMESPACE_NAME}" -o name)" -- /bin/bash
